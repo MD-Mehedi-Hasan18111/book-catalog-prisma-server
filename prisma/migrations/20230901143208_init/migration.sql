7e91f99a3899b98ea292cbe857f8f1544c4f6745
MD Mehedi Hasan <md@bursement.com>
2023-09-01 20:59:28 +0600

    Modify the schema and make auth module sign up and sign in user api

--- prisma/migrations/20230901143208_init/migration.sql
+++ prisma/migrations/20230901143208_init/migration.sql
@@ -1,5 +1,16 @@
--- CreateEnum
-CREATE TYPE "OrderStatus" AS ENUM ('pending', 'shipped', 'delivered');
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+    "contactNo" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "profileImg" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateTable
 CREATE TABLE "Category" (
@@ -38,11 +49,14 @@ CREATE TABLE "Order" (
     "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "orderedBooks" JSONB NOT NULL,
-    "status" "OrderStatus" NOT NULL DEFAULT 'pending',
+    "status" TEXT NOT NULL DEFAULT 'pending',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- AddForeignKey
 ALTER TABLE "Book" ADD CONSTRAINT "Book_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/app/modules/auth/auth.service.ts
@@ -0,0 +1,74 @@
+import { PrismaClient } from '.prisma/client';
+import { User } from '@prisma/client';
+import bcrypt from 'bcrypt';
+import httpStatus from 'http-status';
+import { Secret } from 'jsonwebtoken';
+import config from '../../../config';
+import ApiError from '../../../errors/ApiError';
+import { jwtHelpers } from '../../../helpers/jwtHelpers';
+
+const prisma = new PrismaClient();
+
+export const signUpUser = async (userData: User): Promise<User> => {
+  // Check if the email is already registered
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      email: userData?.email,
+    },
+  });
+
+  if (existingUser) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Email is already in use');
+  }
+
+  // Hash the user password
+  const hashedPassword = await bcrypt.hash(userData?.password, 10);
+
+  const newUser = await prisma.user.create({
+    data: {
+      ...userData,
+      password: hashedPassword,
+    },
+  });
+
+  return newUser;
+};
+
+export const SignInUser = async (userCredential: {
+  email: string;
+  password: string;
+}): Promise<string> => {
+  // check user
+  const existUser = await prisma.user.findUnique({
+    where: {
+      email: userCredential?.email,
+    },
+  });
+
+  if (!existUser) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Email does not exist!');
+  }
+
+  const isPasswordMatched = await bcrypt.compare(
+    userCredential?.password,
+    existUser?.password
+  );
+
+  if (!isPasswordMatched) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Password is incorrect!');
+  }
+
+  // create token
+  const Token = jwtHelpers.createToken(
+    { role: existUser?.role, userId: existUser?.id },
+    config.jwt.secret as Secret,
+    config.jwt.expires_in as string
+  );
+
+  return Token;
+};
+
+export const AuthService = {
+  signUpUser,
+  SignInUser,
+};
